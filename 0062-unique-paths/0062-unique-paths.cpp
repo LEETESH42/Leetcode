783a3154e8d9d047631f7e782d312e87343e9f64
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-15 01:55:23 +0530

    Time: 0 ms (100%), Space: 6.5 MB (31.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,7 +19,18 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return count(m-1,n-1,dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
