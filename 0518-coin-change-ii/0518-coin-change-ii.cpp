af69345a0ae76c39beb2390360dcffb07f8cdc01
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-18 10:48:07 +0530

    Time: 52 ms (29.33%), Space: 18.5 MB (50.83%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-    int count(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
-        if(ind==0){
-            return (amount%coins[0]==0);
-        }
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
-        }
+    // int count(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
+    //     if(ind==0){
+    //         return (amount%coins[0]==0);
+    //     }
+    //     if(dp[ind][amount]!=-1){
+    //         return dp[ind][amount];
+    //     }
 
-        int notTake=count(ind-1,amount,coins,dp);
-        int take=0;
-        if(amount>=coins[ind]){
-            take=count(ind,amount-coins[ind],coins,dp);
-        }
+    //     int notTake=count(ind-1,amount,coins,dp);
+    //     int take=0;
+    //     if(amount>=coins[ind]){
+    //         take=count(ind,amount-coins[ind],coins,dp);
+    //     }
 
-        return dp[ind][amount]=(notTake+take);
-    }
+    //     return dp[ind][amount]=(notTake+take);
+    // }
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
-        return count(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }
+        }
+
+        for(int i=1;i<n;i++){   
+            for(int tar=0;tar<=amount;tar++){
+                int notTake=dp[i-1][tar];
+                int take=0;
+                if(coins[i]<=tar){
+                    take=dp[i][tar-coins[i]];
+                }
+                dp[i][tar]=notTake+take;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
