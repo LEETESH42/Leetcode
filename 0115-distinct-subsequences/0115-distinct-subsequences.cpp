05a1a38842a622108f48a5da9d125e27ea9088b2
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-21 12:17:49 +0530

    Time: 70 ms (25.55%), Space: 30 MB (39.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -24,7 +24,24 @@ public:
         int n=s.size();
         int m=t.size();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,t,dp);
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        // return solve(n-1,m-1,s,t,dp);
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 };
