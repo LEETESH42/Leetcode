7fc616b4abf29253ddbbda2c3c81c3b0e2663f4e
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-24 15:12:42 +0530

    Time: 9 ms (89.08%), Space: 10.3 MB (88.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,19 +15,33 @@ public:
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        // return solve(0,-1,n,nums,dp);
-        int ans=1;
-        vector<int> dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],dp[j]+1);
-                    ans=max(ans,dp[i]);
-                }
+        // int n=nums.size();
+        // // vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        // // return solve(0,-1,n,nums,dp);
+        // int ans=1;
+        // vector<int> dp(n,1);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[i]>nums[j]){
+        //             dp[i]=max(dp[i],dp[j]+1);
+        //             ans=max(ans,dp[i]);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
             }
         }
-        return ans;
+        return temp.size();
+
     }
 };
