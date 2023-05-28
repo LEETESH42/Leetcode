1f9735eb75f7b663a44edb3cf8ea3fcdfdd3e12e
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-28 13:57:34 +0530

    Time: 443 ms (65.19%), Space: 10.4 MB (50.23%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -20,7 +20,22 @@ public:
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(1,n,nums,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        // return solve(1,n,nums,dp);
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+                int mx=INT_MIN;
+                for(int ind=i;ind<=j;ind++){
+                    int cost=nums[i-1]*nums[ind]*nums[j+1]+dp[i][ind-1]+dp[ind+1][j];
+                    mx=max(mx,cost);
+                }
+                dp[i][j]=mx;
+            }
+        }
+        return dp[1][n];
     }
 };
