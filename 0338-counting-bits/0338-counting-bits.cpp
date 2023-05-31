430e71a14b440955de9dd464019749594d9b50ba
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-05-31 05:59:12 +0530

    Time: 10 ms (28.9%), Space: 8.6 MB (5.86%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            int num=i;
+            int sum=0;
+
+            while(num!=0){
+                sum+=num%2;
+                num=num/2;
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
