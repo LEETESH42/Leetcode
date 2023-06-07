e101e43465696c0e5135fc63e61fc8926479ebdc
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-06-07 09:45:58 +0530

    Time: 248 ms (13.32%), Space: 61.6 MB (22.42%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
-    bool dfs(int i, int vis[], int visPath[], int check[], vector<vector<int>>& graph){
-        vis[i]=1;
-        visPath[i]=1;
-
-        check[i]=0;
-
-        for(auto it:graph[i]){
-            if(!vis[it]){
-                if(dfs(it,vis,visPath,check,graph)==true){
-                    check[i]=0;
-                    return true;
-                }
-            }
-            else if(visPath[it]){
-                check[i]=0;
-                return true;
-            }
-        }
-        check[i]=1;
-        visPath[i]=0;
-        return false;
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        int vis[n];
-        int visPath[n];
-        int check[n];
+        vector<vector<int>> revGraph(n);
+        vector<int> indegree(n,0);
 
         for(int i=0;i<n;i++){
-            vis[i]=0;
-            visPath[i]=0;
-            check[i]=0;
-        }
-        vector<int> safeNodes;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,visPath,check,graph);
+            for(auto it:graph[i]){
+                revGraph[it].push_back(i);
+                indegree[i]++;
             }
         }
+
+        queue<int> q;
+        vector<int> ans;
         for(int i=0;i<n;i++){
-            if(check[i]==1){
-                safeNodes.push_back(i);
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        return safeNodes;
+
+        while(!q.empty()){
+            int cur=q.front();
+            q.pop();
+
+            ans.push_back(cur);
+            for(auto it:revGraph[cur]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
