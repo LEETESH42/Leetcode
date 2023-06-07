2680f83e15c8e8532a3c414f5c242df6bbba56f3
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-06-07 17:40:00 +0530

    Time: 0 ms (100%), Space: 5.9 MB (53.28%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int ans=0;
-        while(a||b||c){
-            int x=a&1;
-            int y=b&1;
-            int z=c&1;
+        for(int i=0;i<=30;i++){
+            int d1=a&1;
+            int d2=b&1;
+            int d3=c&1;
 
-            if((x|y)!=z){
-                if(x&y){
-                    ans+=2;
-                }
-                else{
-                    ans+=1;
+            if(d3==0){
+                ans+=d1+d2;
+            }
+            else{
+                if(d1==0 && d2==0){
+                    ans++;
                 }
             }
-            a=a>>1;
-            b=b>>1;
-            c=c>>1;
+            a/=2;
+            b/=2;
+            c/=2;
         }
         return ans;
     }
