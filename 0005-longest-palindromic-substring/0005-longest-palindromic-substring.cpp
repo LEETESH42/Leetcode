d323321710f1e7297ab85d0d3e58032fca9856e3
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-06-19 09:16:59 +0530

    Time: 332 ms (35.01%), Space: 292 MB (10.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
-    string ans="";
-    void expand(int i, int j, string s){
-        while(i>=0 && j<s.size() && s[i]==s[j]){
-            i--;
-            j++;
-        }
-        if(ans.size()<j-i){
-            ans=s.substr(i+1,j-i-1);
-        }
-    }
-
     string longestPalindrome(string s) {
-        int mx=1;
-        for(int i=0;i<s.size();i++){
-            expand(i,i+1,s);
-            expand(i,i,s);
+        string longest="";
+        int len=0;
+        int n=s.size();
+        if(n==1){
+            return s;
         }
-        return ans;
+        vector<vector<int>> dp(n,vector<int>(n+10,0));
+        int x=0,y=0;
+
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=2;
+                x=i;
+                y=i+1;
+                len=2;
+            }
+        }
+
+        for(int diff=2;diff<n;diff++){
+            for(int i=0;(i+diff)<n;i++){
+                if(s[i]==s[i+diff] && dp[i+1][i+diff-1]){
+                    dp[i][i+diff]=dp[i+1][i+diff-1]+1;
+                    if(diff+1>len){
+                        x=i;
+                        y=diff+i;
+                        len=dp[i][i+diff];
+                    }
+                }
+                
+            }
+        }
+        longest=s.substr(x,y-x+1);
+        if(longest.empty()){
+            return longest=s[0];
+        }
+        else return longest;
+
     }
 };
