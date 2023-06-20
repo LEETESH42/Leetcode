7a4989f9715ee03f31ad0f7494a9faa9d01a573f
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-06-20 10:49:34 +0530

    Time: 418 ms (42.27%), Space: 14.3 MB (51.68%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -3,9 +3,10 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         queue<pair<string,int>> q;
         q.push({beginWord,1});
+       
         set<string> st(wordList.begin(),wordList.end());
-
         st.erase(beginWord);
+        
         while(!q.empty()){
             string word=q.front().first;
             int steps=q.front().second;
@@ -16,15 +17,15 @@ public:
             }
 
             for(int i=0;i<word.size();i++){
-                char original=word[i];
-                for(char ch='a';ch<='z';ch++){
-                    word[i]=ch;
+                char org=word[i];
+                for(char c='a';c<='z';c++){
+                    word[i]=c;
                     if(st.find(word)!=st.end()){
                         st.erase(word);
                         q.push({word,steps+1});
                     }
                 }
-                word[i]=original;
+                word[i]=org;
             }
         }
         return 0;
