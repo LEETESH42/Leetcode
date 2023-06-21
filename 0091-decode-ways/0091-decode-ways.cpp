a12f485a2400678f0cc7b7a25a7fc5ce96702693
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-06-21 12:50:58 +0530

    Time: 0 ms (100%), Space: 7.6 MB (15.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(string s, int ind,vector<int>& dp){
-        if(ind==s.size()){
+    int solve(int index, string s, vector<int>& dp){
+        if(index==s.size()){
             return 1;
         }
-        if(dp[ind]!=-1){
-            return dp[ind];
+        if(dp[index]!=-1){
+            return dp[index];
         }
         int ans=0;
-        if(s[ind]!='0'){
-            ans+=solve(s,ind+1,dp);
+        if(s[index]!='0'){
+            ans+=solve(index+1,s,dp);
         }
-        if(ind+1<s.size() && (s[ind]=='1' || (s[ind]=='2' && s[ind+1]<='6'))){
-            ans+=solve(s,ind+2,dp);
+        if(index+1<s.size() && (s[index]=='1' ||(s[index]=='2'&& s[index+1]<='6'))){
+            ans+=solve(index+2,s,dp);
         }
-        return dp[ind]=ans;
+        return dp[index]=ans;
     }
 
     int numDecodings(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return solve(s,0,dp);
+        vector<int> dp(n+1,-1);
+        return solve(0,s,dp);
     }
 };
