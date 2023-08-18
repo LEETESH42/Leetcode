0f1acf238c5414b063504b8d7eb66345684a7849
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-08-18 16:40:48 +0530

    Time: 65 ms (95.83%), Space: 40.9 MB (82.04%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,75 +1,29 @@
 class Solution {
 public:
-    vector<int> solve(vector<int> arr){
-        int n=arr.size();
-        vector<int> res(n);
-
-        stack<int> st;
-
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                res[i]=n;
-            }
-            else{
-                while(!st.empty() && arr[st.top()]>arr[i]){
-                    st.pop();
-                }
-                if(!st.empty()){
-                    res[i]=st.top();
-                }
-                else{
-                    res[i]=n;
-                }
-            }
-            st.push(i);
-        }
-        return res;
-    }
-
-    vector<int> get(vector<int> arr){
-        int n=arr.size();
-        vector<int> res(n);
-
-        stack<int> st;
-
-        for(int i=0;i<n;i++){
-            if(st.empty()){
-                res[i]=-1;
-            }
-            else{
-                while(!st.empty() && arr[st.top()]>=arr[i]){
-                    st.pop();
-                }
-                if(!st.empty()){
-                    res[i]=st.top();
-                }
-                else{
-                    res[i]=-1;
-                }
-            }
-            st.push(i);
-        }
-        return res;
-    }
-
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int> nse=solve(arr);
-        vector<int> pse=get(arr);
-
         int n=arr.size();
+        vector<int> ans(n,0);
 
-        long long sum=0;
-        int m=1e9+7;
+        int sum=0;
+        int mod=1e9+7;
+
+        stack<int> st;
 
         for(int i=0;i<n;i++){
-            long long ps=i-pse[i];
-            long long ns=nse[i]-i;
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            int sum1=0;
 
-            long long total=ps*ns;
-
-            long long tot=arr[i]*total;
-
-            sum=(sum+tot)%m;
+            if(st.empty()){
+                sum1=(sum1+(i+1)*arr[i])%mod;
+            }
+            else{
+                sum1=(sum1+(i-st.top())*arr[i] +ans[st.top()])%mod;
+            }
+            ans[i]=sum1;
+            sum=(sum+sum1)%mod;
+            st.push(i);
         }
         return sum;
     }
