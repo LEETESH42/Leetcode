bf17da7e62aafe3c284147598c42df1780d7eafe
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-08-25 18:02:52 +0530

    Time: 534 ms (79.92%), Space: 155.8 MB (31.76%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>& events, vector<int>& start, int i, int k, vector<vector<int>>& dp){
+        if(i==events.size()||k==0){
+            return 0;
+        }
+        if(dp[i][k]!=-1){
+            return dp[i][k];
+        }
+
+        int nt=solve(events,start,i+1,k,dp);
+
+        auto x=upper_bound(start.begin(),start.end(),events[i][1])-start.begin();
+        int t=events[i][2]+solve(events, start, x,k-1,dp);
+        return dp[i][k]=max(t,nt);
+    }
+
     int maxTwoEvents(vector<vector<int>>& events) {
         int n=events.size();
-        priority_queue<pair<int,pair<int,int>>> pq;
+        vector<vector<int>> dp(n+1, vector<int>(3,-1));
 
-        int mxvalue=0;
-        int ans=0;
-
+        vector<int> start;
         sort(events.begin(),events.end());
 
-        for(int i=0;i<n;i++){
-            //take out all events which have ending time lesser than the current time and take maximum
-            while(!pq.empty() &&(-1*pq.top().first)<events[i][0]){
-                mxvalue=max(mxvalue,pq.top().second.second);
-                pq.pop();
-            }
-            ans=max(ans,mxvalue+events[i][2]);
-            pq.push({-1*events[i][1],{events[i][0],events[i][2]}});
+        for(int i=0;i<events.size();i++){
+            start.push_back(events[i][0]);
         }
-        return ans;
+
+        return solve(events,start,0,2,dp);
     }
 };
