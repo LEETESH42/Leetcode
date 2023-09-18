0bc716dee760c15f713861902898d2c16366e0e1
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-09-18 10:51:20 +0530

    Time: 3 ms (32.7%), Space: 6.6 MB (15.18%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
+    int solve(int n, vector<int> & dp){
         if(n==0 || n==1){
             return 1;
         }
@@ -17,19 +17,7 @@ public:
     }
 
     int integerBreak(int n) {
-        vector<int> dp(n+1,0);
-        // return solve(n,dp);
-
-        dp[0]=1;
-        dp[0]=1;
-        for(int i=2;i<=n;i++){
-            int mx=1;
-            for(int j=1;j<i;j++){
-                int temp=max(j*(i-j), j*dp[i-j]);
-                mx=max(mx,temp);
-            }
-            dp[i]=mx;
-        }
-        return dp[n];
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
