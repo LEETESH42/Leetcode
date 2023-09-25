8390483951064b589e49eac41ba82421bb090e2d
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-09-25 16:01:07 +0530

    Time: 0 ms (100%), Space: 6.3 MB (57.86%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    int dp[1001][1001];
-
-    int solve(int curr, int c, int n){
-        if(curr==n){
-            return 0;
-        }
-
-        if(dp[curr][c]!=-1){
-            return dp[curr][c];
-        }
-
-        int copy=1e9, paste=1e9;
-
-        if(curr!=c){
-            copy=1+solve(curr,curr,n);
-        }
-
-        if(curr+c<=n){
-            paste=1+solve(curr+c, c, n);
-        }
-        return dp[curr][c]=min(copy,paste);
-    }
-
     int minSteps(int n) {
-        memset(dp,-1,sizeof(dp));
-        if(n==1){
-            return 0;
+        int ans=0;
+        for(int i=2;i*i<=n;){
+            if(n%i==0){
+                ans+=i;
+                n/=i;
+            }
+            else{
+                i++;
+            }
         }
-        if(n==2){
-            return 2;
+        if(n!=1){
+            ans+=n;
         }
-        return 2+solve(2,1,n);
+        return ans;
     }
 };
