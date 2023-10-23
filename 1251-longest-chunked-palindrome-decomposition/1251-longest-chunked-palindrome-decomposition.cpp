ada23960d5719549f924403fe2a669fd143b2004
LEETESH42 <75121758+LEETESH42@users.noreply.github.com>
2023-10-23 14:44:18 +0530

    Time: 4 ms (65.9%), Space: 9.5 MB (53.44%) - LeetHub

--- 1251-longest-chunked-palindrome-decomposition/1251-longest-chunked-palindrome-decomposition.cpp
+++ 1251-longest-chunked-palindrome-decomposition/1251-longest-chunked-palindrome-decomposition.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    long long dp[1001][1001];
-    long long solve(int st, int end, string& text){
-        if(st>end){
-            return 0;
-        }
+    int longestDecomposition(string text) {
+        int n=text.size();
+        int result=0;
 
-        if(st==end){
-            return 1;
-        }
-        if(dp[st][end]!=-1){
-            return dp[st][end];
-        }
+        string left="";
+        string right="";
 
-        long long mx=1;
-        int n=end-st+1;
-
-        for(int i=1;i<=n/2;i++){
-            string left=text.substr(st,i);
-            string right=text.substr(end-i+1,i);
+        for(int i=0;i<n;i++){
+            left+=text[i];
+            right=text[n-i-1]+right;
             if(left==right){
-                mx=max(mx,2+solve(st+i,end-i,text));
+                result++;
+                left="";
+                right="";
             }
         }
-        return dp[st][end]=mx;
-    }
-    int longestDecomposition(string text) {
-        memset(dp,-1,sizeof(dp));
-        int n=text.size();
-        return solve(0,n-1,text);
+        return result;
     }
 };
